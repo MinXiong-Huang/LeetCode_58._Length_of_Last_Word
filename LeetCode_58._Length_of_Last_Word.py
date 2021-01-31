fe8f2276cda8e08b91326a2ff06f0a84b36ada16
黃敏雄 <64833384+MinXiong-Huang@users.noreply.github.com>
2021-01-31 12:27:42 +0800

    Update LeetCode_58._Length_of_Last_Word.py

--- LeetCode_58._Length_of_Last_Word.py
+++ LeetCode_58._Length_of_Last_Word.py
@@ -4,12 +4,12 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        if len(s)==0:
+        if len(s) == 0:
             return 0
         
-        s=s.rstrip(' ').split(' ')[-1] #rstrip:刪除string字符串末尾的指定字符後生成的新字符串
+        s = s.rstrip(' ').split(' ')[-1] #rstrip:刪除string字符串末尾的指定字符後生成的新字符串
         return len(s)
 
 #Example
-s=Solution()
+s = Solution()
 s.lengthOfLastWord("Hello World")
